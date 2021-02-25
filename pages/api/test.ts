ed1f84ce5d04fae1a1085335a68ecd0cfacf757e
jamesBeeProg <jamesBeeProg@gmail.com>
2021-02-25 12:59:54 -0500

    More debugging

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -2,5 +2,6 @@ import { prisma } from "../../lib/prisma";
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default async (req: NowRequest, res: NowResponse) => {
+    console.log(process.env.DATABASE_URL)
     return await prisma.entry.findMany();
 };
