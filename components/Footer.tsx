1319d4b9d9303ad32fa5b3c45c68e20e20e19aa8
wallaby14 <wjallaby@gmail.com>
2021-03-08 14:15:49 -0500

    Some changes to main website, Done for the day

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,20 @@ export function Footer() {
     return (
         <footer>
             <hr />
-            Footer text
+            Logo Designer: Mylena Bovo, Coders: James Clark, Wyatt Allaby
             <style jsx>{`
                 footer {
                     text-align: center;
                     padding-top: 1em;
                     padding-bottom: 1em;
+                    font-style: italic;
+                }
+                footer hr {
+                    display: block;
+                    text-align: center;
+                    background-color: navy;
+                    height: 0.2em;
+                    width: 75vw
                 }
             `}</style>
         </footer>
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,26 +11,60 @@ export function Header({ year = '' }) {
             </Head>
             <h1>Step Competition {yearToString(year)}</h1>
             <hr />
-            <nav>
+            <nav className="tab">
                 <Link href="/">
-                    Everyone
+                    <a>Everyone</a>
                 </Link>
-                {years.map(year => (
-                    <Link href={`/${year}`} key={year}>
-                        {yearToString(year)}
+                {years.map(y => (
+                    <Link href={`/${y}`} key={y}>
+                        <a className={y === year ? 'active' : ''}>{yearToString(y)}</a>
                     </Link>
                 ))}
             </nav>
             <hr />
             <style jsx>{`
+
+                @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+
                 h1 {
                     text-align: center;
                 }
 
+                hr {
+                    display: block;
+                    text-align: center;
+                    background-color: navy;
+                    height: 0.2em;
+                }
+
                 nav {
-                    padding: 1em;
+                    font-family: 'Anton', sans-serif;
                     display: flex;
                     justify-content: space-around;
+                    background-color: inherit;
+                    color: navy;
+                    border: 0.3em;
+                    outline: none;
+                    padding: 1em;
+                    cursor: pointer;
+                    transition: 0.3s;
+                    font-size: 17px;
+                }
+
+                .tab {
+                    overflow: hidden;
+                    border: 1px solid white;
+                    background-color: white;
+                }
+
+                /* Change background color of buttons on hover */
+                a:hover {
+                    background-color: gold;
+                }
+
+                /* Create an active/current tablink class */
+                a.active {
+                    background-color: gold;
                 }
             `}</style>
         </header>
