2d6a6e17137d364a6c66324032634998eaee2171
wallaby14 <wjallaby@gmail.com>
2021-03-22 13:06:08 -0400

    Prisma fix for build errors, list

--- administration/list.tsx
+++ administration/list.tsx
@@ -1,18 +1,20 @@
 import { InferGetStaticPropsType } from 'next';
 import Link from 'next/link';
 import React from 'react';
-import { prisma } from '../lib/prisma';
+import { usePrisma } from '../lib/prisma';
 
 export const getStaticProps = async () => {
     // Prisma can't `distinct` and `groupBy` at the same time so we do it manually
-    const query = await prisma.entry.findMany({
-        select: {
-            shop: true,
-            name: true,
-            year: true,
-        },
-        distinct: ['name', 'year'],
-    });
+    const query = await usePrisma((prisma) =>
+        prisma.entry.findMany({
+            select: {
+                shop: true,
+                name: true,
+                year: true,
+            },
+            distinct: ['name', 'year'],
+        }),
+    );
 
     return {
         props: {},
