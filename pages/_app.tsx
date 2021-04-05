0a1ccd6da29aaa4497401cc88561ab0a1b39fb25
jamesBeeProg <jamesBeeProg@gmail.com>
2021-04-05 10:13:16 -0400

    Extract _app CSS into own file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,7 @@
 import { AppProps } from 'next/app';
 import React from 'react';
+import './_app.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Component {...pageProps} />
-            <style jsx global>{`
-                @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-
-                html,
-                body {
-                    margin: 0;
-                    padding: 0;
-                }
-
-                body {
-                    font-family: 'Open Sans', sans-serif;
-                }
-            `}</style>
-        </>
-    );
+    return <Component {...pageProps} />;
 }
