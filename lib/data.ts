b5b72bbbc6f7746c9eb322d8de2431c6aaf53851
jamesBeeProg <jamesBeeProg@gmail.com>
2021-04-06 13:53:23 -0400

    Start week range logic

--- lib/data.ts
+++ lib/data.ts
@@ -27,3 +27,17 @@ export const years = [2021, 2022, 2023, 2024] as const;
 export const stringYears = years.map(String);
 
 export const times = ['All Time', 'This Week', 'Last Week'] as const;
+
+const hour = 1000 * 60 * 60;
+const week = hour * 24 * 7;
+const utcOffset = -(hour * 4);
+
+const trueStartDate = +new Date(2021, 2, 12);
+const adjustedStartDate = trueStartDate + utcOffset;
+
+export const getWeekRange = (weeksSinceStart: number) => {
+    const start = adjustedStartDate + week * weeksSinceStart;
+    const end = start + week;
+
+    return { start, end } as const;
+};
