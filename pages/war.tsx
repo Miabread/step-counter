c10d6050d1bc1fbfd9ce3bd890606eab911cc140
jamesBeeProg <jamesBeeProg@gmail.com>
2021-05-25 13:31:06 -0400

    Create war page

---
+++ pages/war.tsx
@@ -0,0 +1,62 @@
+import { usePrisma } from '../lib/prisma';
+import Head from 'next/head';
+import React from 'react';
+import { InferGetStaticPropsType } from 'next';
+import { filterByTime } from '../lib/time';
+import { DataHeader } from '../components/DataHeader';
+import { createStyle } from '../lib/misc';
+import css from './war.module.scss';
+
+const style = createStyle(css);
+
+export const getStaticProps = async () => {
+    const [cpwd, health] = await usePrisma(async (prisma) =>
+        Promise.all([
+            prisma.entry.aggregate({
+                where: {
+                    year: { not: 0 },
+                    shop: 6,
+                    verified: true,
+                    ...filterByTime('all'),
+                },
+                sum: { steps: true },
+            }),
+            prisma.entry.aggregate({
+                where: {
+                    year: { not: 0 },
+                    shop: 14,
+                    verified: true,
+                    ...filterByTime('all'),
+                },
+                sum: { steps: true },
+            }),
+        ]),
+    );
+
+    return {
+        props: { cpwd, health },
+        revalidate: 60,
+    };
+};
+
+export default function War({
+    cpwd,
+    health,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+    return (
+        <div className={style('grid-container')}>
+            <Head>
+                <title>War | Step Into Action</title>
+            </Head>
+            <DataHeader />
+            <div className={style('left', 'center')}>
+                <h1>CPWD</h1>
+                <h2>{cpwd.sum.steps.toLocaleString()}</h2>
+            </div>
+            <div className={style('right', 'center')}>
+                <h1>Health Tech</h1>
+                <h2>{health.sum.steps.toLocaleString()}</h2>
+            </div>
+        </div>
+    );
+}
